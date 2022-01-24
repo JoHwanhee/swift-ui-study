5d64a25d526e4d0966b0a7aabb7a982696aec518
조환희 <johwanhui@johwanhuis-Mac-mini.local>
2022-01-25 00:08:13 +0900

    ferture: add v2

---
+++ study2/Shared/study2App.swift
@@ -0,0 +1,17 @@
+//
+//  study2App.swift
+//  Shared
+//
+//  Created by 조환희 on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct study2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ui-move/ui-move/ui_moveApp.swift
@@ -0,0 +1,18 @@
+//
+//  ui_moveApp.swift
+//  ui-move
+//
+//  Created by 조환희 on 2022/01/23.
+//
+//
+
+import SwiftUI
+
+@main
+struct ui_moveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
